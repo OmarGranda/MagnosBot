b980e6656815206abcbf344d84edda9415fce2c3
OmarGranda <omargranda673@gmail.com>
2025-09-18 14:23:09 -0500

    Actualizar group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -3,19 +3,19 @@ const handler = async (m, { conn }) => {
     // let who = m?.message?.extendedTextMessage?.contextInfo?.participant || m?.mentionedJid[0] || await m?.quoted?.sender;
    let texto = await m.mentionedJid
    let who = texto.length > 0 ? texto[0] : (m.quoted ? await m.quoted.sender : false)
-    if (!who) return m.reply('🕸 Menciona al usuario que deseas promover a administrador.');
+    if (!who) return m.reply('🫂 Menciona al usuario que deseas promover a administrador.');
 
     const groupMetadata = await conn.groupMetadata(m.chat);
     const participant = groupMetadata.participants.find(participant => participant.jid === who);
 
     if (participant && participant.admin) {
-    return conn.reply(m.chat, `🕸 *@${who.split('@')[0]}* ya es administrador del grupo!`, m, { mentions: [who] });
+    return conn.reply(m.chat, `🫵 *@${who.split('@')[0]}* ya es administrador del grupo!`, m, { mentions: [who] });
     }
 
     await conn.groupParticipantsUpdate(m.chat, [who], 'promote');
-    await conn.reply(m.chat, `🕸 *@${who.split('@')[0]}* ha sido promovido a administrador del grupo!`, m, { mentions: [who] });
+    await conn.reply(m.chat, `😎 *@${who.split('@')[0]}* ha sido promovido a administrador del grupo!`, m, { mentions: [who] });
   } catch (e) {
-    await m.reply(`🌾 Error.`);
+    await m.reply(` ❌ Error.`);
   }
 };
 
