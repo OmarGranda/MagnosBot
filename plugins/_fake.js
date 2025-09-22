e9f35794ed4a0528898f503a04703d959c8e98af
𓆩⚡ ✞ 𝑺𝒉𝒂𝒅𝒐𝒘 𝒐𝒇𝒇𝒊𝒄𝒊𝒂𝒍 🍧𓆪͟͞ <blackoficial2025@gmail.com>
2025-09-21 22:42:16 -0500

    Update _fake.js

--- plugins/_fake.js
+++ plugins/_fake.js
@@ -1,40 +1,60 @@
 export async function before(m, { conn }) {
 
-var canal = 'https://whatsapp.com/channel/0029VbApwZ9ISTkEBb6ttS3F'
-var canal2 = 'https://whatsapp.com/channel/0029Vb6IdnEGU3BTahqaLL2V'
-var canal3 = 'https://whatsapp.com/channel/0029VbBVnhSBlHpUWKo19m2m'
-var api = 'https://api.stellarwa.xyz'
-var git = 'https://github.com/DevAlexJs'
-var md = 'https://github.com/DevAlexJs/SakuraBot-MD'
+  const canal = 'https://whatsapp.com/channel/0029VbApwZ9ISTkEBb6ttS3F'
+  const canal2 = 'https://whatsapp.com/channel/0029Vb6IdnEGU3BTahqaLL2V'
+  const canal3 = 'https://whatsapp.com/channel/0029VbBVnhSBlHpUWKo19m2m'
+  const api = 'https://api.stellarwa.xyz'
+  const git = 'https://github.com/DevAlexJs'
+  const md = 'https://github.com/DevAlexJs/SakuraBot-MD'
 
-globalThis.redes = [canal, canal2, canal3, api, git, md].getRandom()
+  const redes = [canal, canal2, canal3, api, git, md]
+  const randomRed = redes[Math.floor(Math.random() * redes.length)]
 
-  const canales = Object.entries(global.my)
-  .reduce((acc, [key, value]) => {
-    if (key.startsWith('ch')) {
-      const index = key.slice(2)
-      const nombre = global.my[`name${index}`]
-      if (nombre) {
-        acc.push({ id: value, nombre })
+  const canales = Object.entries(global.my || {})
+    .reduce((acc, [key, value]) => {
+      if (key.startsWith('ch')) {
+        const index = key.slice(2)
+        const nombre = global.my[`name${index}`]
+        if (nombre) acc.push({ id: value, nombre })
+      }
+      return acc
+    }, [])
+
+  const channelRD = canales[Math.floor(Math.random() * canales.length)] || { id: '120363422169517881@newsletter', nombre: '🎋 MagnosBot | CHANNEL 🌷' }
+
+  const icon = 'https://stellarwa.xyz/files/1757378468505.jpeg'
+  const thumbnail = await (await fetch(icon)).arrayBuffer()
+
+  global.fake = {
+    contextInfo: {
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: channelRD.id,
+        serverMessageId: '',
+        newsletterName: channelRD.nombre
+      },
+      externalAdReply: {
+        title: botname,
+        body: dev,
+        mediaUrl: null,
+        description: null,
+        previewType: "PHOTO",
+        thumbnail: Buffer.from(thumbnail),
+        sourceUrl: randomRed,
+        mediaType: 1,
+        renderLargerThumbnail: false
       }
     }
-    return acc
-  }, [])
+  }
 
-const channelRD = canales[Math.floor(Math.random() * canales.length)]
-
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '0', newsletterName: channelRD.nombre, }, externalAdReply: { showAdAttribution: true, title: packname, body: 'xd', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icon, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }}}
-
-  
-global.rcanal = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: channelRD.id,
-      newsletterName: channelRD.nombre,
-      serverMessageId: '0'
+  global.rcanal = {
+    contextInfo: {
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: channelRD.id,
+        newsletterName: channelRD.nombre,
+        serverMessageId: '0'
+      }
     }
   }
-}
-
-}
+    }
